e7048375b6af9a027b517d5c3e83a7de78dfcd79
Heron Sousa <heronrs1@gmail.com>
2022-07-01 00:42:54 -0300

    test: :white_check_mark: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,7 +1,9 @@
 import { HttpPostClientSpy } from '@/data/test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
-import { faker } from '@faker-js/faker'
 import { mockAuthentication } from '@/domain/test/mock-authentication'
+import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { faker } from '@faker-js/faker'
 
 type SutTypes = {
     httpPostClientSpy: HttpPostClientSpy
@@ -35,5 +37,18 @@ describe('RemoteAuthentication', () => {
         await sut.auth(authenticationParams)
 
         expect(httpPostClientSpy.body).toEqual(authenticationParams)
+    })
+
+    test('Should throw InvalidCredentialsError HttpPostClient returns 401', async () => {
+        const authenticationParams = mockAuthentication()
+        const { sut, httpPostClientSpy } = makeSut()
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unathorized
+        }
+
+        const promise = sut.auth(authenticationParams)
+
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError())
     })
 })
