83e000bd231afdc09e4dca9e9fa4df4980f48258
Heron Sousa <heronrs1@gmail.com>
2022-07-01 00:50:59 -0300

    test: :white_check_mark: ensure RemoteAuthentication throws UnepectedError on default cases

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,6 +4,7 @@ import { mockAuthentication } from '@/domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { faker } from '@faker-js/faker'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 
 type SutTypes = {
     httpPostClientSpy: HttpPostClientSpy
@@ -50,5 +51,44 @@ describe('RemoteAuthentication', () => {
         const promise = sut.auth(authenticationParams)
 
         await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+    })
+
+    test('Should throw Unexpectederror HttpPostClient returns 400', async () => {
+        const authenticationParams = mockAuthentication()
+        const { sut, httpPostClientSpy } = makeSut()
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.badRequest
+        }
+
+        const promise = sut.auth(authenticationParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+    test('Should throw Unexpectederror HttpPostClient returns 500', async () => {
+        const authenticationParams = mockAuthentication()
+        const { sut, httpPostClientSpy } = makeSut()
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.serverError
+        }
+
+        const promise = sut.auth(authenticationParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
+    test('Should throw Unexpectederror HttpPostClient returns 404', async () => {
+        const authenticationParams = mockAuthentication()
+        const { sut, httpPostClientSpy } = makeSut()
+
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.notFound
+        }
+
+        const promise = sut.auth(authenticationParams)
+
+        await expect(promise).rejects.toThrow(new UnexpectedError())
     })
 })
