3102c3db146488c69694e06420e763fb1a5d4570
Heron Sousa <heronrs1@gmail.com>
2022-07-05 00:05:40 -0300

    test: :white_check_mark: ensure AxiosHttpClient returns correct values

--- src/infra/http/axiosHttpClient/axios-http-client.spec.ts
+++ src/infra/http/axiosHttpClient/axios-http-client.spec.ts
@@ -6,6 +6,13 @@ import { HttpPostParams } from '@/data/protocols/http'
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
 
+const mockedAxiosResult = {
+    data: faker.animal.dog(),
+    status: faker.random.numeric()
+}
+
+mockedAxios.post.mockResolvedValue(mockedAxiosResult)
+
 const makeSut = (): AxiosHttpClient => {
     return new AxiosHttpClient()
 }
@@ -22,5 +29,15 @@ describe('AxiosHttpClient', () => {
 
         await sut.post(request)
         expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
+    })
+
+    test('Should return the correct statusCode and body', async () => {
+        const sut = makeSut()
+
+        const response = await sut.post(mockPostRequest())
+        expect(response).toEqual({
+            statusCode: mockedAxiosResult.status,
+            body: mockedAxiosResult.data
+        })
     })
 })
